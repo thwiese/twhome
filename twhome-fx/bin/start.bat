5f7b94e2479a070d6ea3c6650b09340041dc8e65
Thomas Wiese <mail@thomaswiese.de>
2021-12-03 13:11:36 +0100

    add windows start script

---
+++ twhome-fx/bin/start.bat
@@ -0,0 +1,6 @@
+@echo off
+set JAVA_HOME=C:\dev\java\jdk-17.0.1
+set JAVAFX_HOME=C:\dev\java\javafx-sdk-17.0.1
+
+%JAVA_HOME%\bin\java -version
+%JAVA_HOME%\bin\java.exe --module-path=%JAVAFX_HOME%\lib --add-modules=javafx.controls -jar ../target/twhome-fx-1.0-SNAPSHOT-shaded.jar
