4160bcff26295060d77b2b96c11ef66dd5e48a60
Thomas Wiese <mail@thomaswiese.de>
2021-12-12 23:19:42 +0100

    simplie UI adjustments

--- twhome-fx/src/main/java/de/twiese/twhome/fx/Starter.java
+++ twhome-fx/src/main/java/de/twiese/twhome/fx/Starter.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
@@ -12,8 +13,14 @@ public class Starter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("TW Home");
+        stage.setMaximized(true);
         Pane pane = new StackPane();
+        pane.setStyle("-fx-background-color: #000000;");
+
         Label label = new Label("Guten Morgen,  Familie Wiese");
+        label.setTextFill(Color.WHITE);
+        label.setStyle("-fx-font-size: 64px");
+
         pane.getChildren().add(label);
         Scene scene = new Scene(pane,800, 600);
         stage.setScene(scene);
