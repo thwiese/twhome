13f8ba5cc33ecfaa6ca759aa93bed05cc2417ec2
Thomas Wiese <mail@thomaswiese.de>
2021-12-20 20:54:01 +0100

    setup twhome-be spring boot project

---
+++ twhome-be/src/main/java/de/twiese/twhome/be/TwhomeBeApplication.java
@@ -0,0 +1,13 @@
+package de.twiese.twhome.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwhomeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwhomeBeApplication.class, args);
+    }
+
+}
---
+++ twhome-be/src/test/java/de/twiese/twhome/be/TwhomeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.twiese.twhome.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwhomeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
