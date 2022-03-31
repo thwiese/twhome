7a21ad16f539f8e3deb8376b7dd417667428215e
Thomas Wiese <mail@thomaswiese.de>
2022-03-31 22:21:54 +0200

    define max image size

--- twhome-fx/src/main/java/de/twiese/twhome/fx/support/ImageTool.java
+++ twhome-fx/src/main/java/de/twiese/twhome/fx/support/ImageTool.java
@@ -1,12 +1,12 @@
 package de.twiese.twhome.fx.support;
 
+import de.twiese.twhome.fx.config.Config;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
@@ -14,10 +14,18 @@ import java.net.URI;
 public class ImageTool {
 
     private static final Logger log = LoggerFactory.getLogger(ImageTool.class);
+    private static final int MAX_IMAGE_SIZE = Config.getIntProperty("maxImageSize");
 
     public static BufferedImage getScaledImage(URI fileUri, int width) throws IOException {
         BufferedImage img = ImageIO.read(fileUri.toURL());
+        if (width > MAX_IMAGE_SIZE) {
+            width = MAX_IMAGE_SIZE;
+        }
         int height = img.getHeight() * width / img.getWidth();
+        if (height > MAX_IMAGE_SIZE) {
+            height = MAX_IMAGE_SIZE;
+            width = img.getWidth() * height / img.getHeight();
+        }
         log.info("Reading image {} (width: {}, height: {}, scaling to width: {}, height: {})", fileUri, img.getWidth(), img.getHeight(), width, height);
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         bufferedImage.getGraphics()
